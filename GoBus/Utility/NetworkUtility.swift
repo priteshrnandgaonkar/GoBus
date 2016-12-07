7117fba2231691946318bec98dde56dbda639b60
Pritesh Nandgaonkar <pncooldude91@gmail.com>
2016-12-07 14:51:55 +0530

    added network utility class

--- GoBus/Utility/NetworkUtility.swift
+++ GoBus/Utility/NetworkUtility.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+//
+//enum NetworkError: Error {
+//    case dataNil
+//    case noResponse
+//    case
+//}
+//struct NetworkUtility {
+//    static func get(url: String, withCompletion completion: (Result<Data, NetworkError>)) {
+//        
+//        let request = URLRequest(url: url, method: .get)
+//        URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error: Error?) in
+//            
+//            guard let response: HTTPURLResponse = response as? HTTPURLResponse && response.statusCode == 200 {
+//                
+//            }
+//        })
+//    }
+//}
